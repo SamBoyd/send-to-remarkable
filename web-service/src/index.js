464f6dc27233eb3cc88ffc2560fea144092cddbc
Samuel Boyd <samboyd10@gmail.com>
2018-04-16 15:14:48 +0100

    Server will now convert passed html to pdf

--- web-service/src/index.js
+++ web-service/src/index.js
@@ -1,11 +1,19 @@
 var http = require('http');
+var wkhtmltox = require('wkhtmltox');
 
 http.createServer(function (req, res) {
+    var converter = new wkhtmltox();
+
+    converter.wkhtmltopdf = "/opt/wkhtmltopdf/wkhtmltox/bin/wkhtmltopdf";
+
     res.writeHead(200, {
         'Content-Type': 'application/pdf',
         'Access-Control-Allow-Origin': '*',
     });
-    res.end("")
+
+    converter.pdf(req, {'load-error-handling': 'ignore'})
+        .pipe(res)
+
 }).listen(1337, '127.0.0.1');
 
 console.log('Server running at http://127.0.0.1:1337/');
--- web-service/test/webServiceTest.js
+++ web-service/test/webServiceTest.js
@@ -1,8 +1,9 @@
 import { describe, it } from 'mocha';
 import { expect, should } from 'chai';
-var { sleep } = require('sleep');
+const { sleep } = require('sleep');
 
-var fs = require('fs')
+const fs = require('fs');
+const path = require('path');
 
 // import nock from 'nock';
 
@@ -12,16 +13,16 @@ const process = require('child_process');
 describe('handleAcknowledge', () => {
 
     it('should start a http server', done => {
-        var server = process.exec('/Users/sam/.nvm/versions/node/v8.9.4/bin/node ./src/index.js',
+        var server = process.exec('node ./src/index.js',
             (error, stdout, stderr) => {
             // console.log('stdout: ', stdout);
             // console.log('stderr: ', stderr);
             // if (error !== null) {
             //     console.log('exec error: ', error);
             // }
-        })
+        });
 
-        sleep(3)
+        sleep(3);
         request
             .post('http://localhost:1337')
             .send("")
@@ -33,27 +34,27 @@ describe('handleAcknowledge', () => {
             .set('Access-Control-Allow-Origin', '*')
             .end((err, res) => {
                 if (err) {
-                    throw new Error(err)
+                    throw new Error(err);
                     done()
                 }
 
-                done()
+                done();
                 server.kill('SIGTERM')
             })
 
-    }).timeout(7000)
+    }).timeout(7000);
 
-    it('should start a http server', done => {
-        var server = process.exec('/Users/sam/.nvm/versions/node/v8.9.4/bin/node ./src/index.js',
+    it('response should have correct headers', done => {
+        var server = process.exec('node ./src/index.js',
             (error, stdout, stderr) => {
                 // console.log('stdout: ', stdout);
                 // console.log('stderr: ', stderr);
                 // if (error !== null) {
                 //     console.log('exec error: ', error);
                 // }
-            })
+            });
 
-        sleep(3)
+        sleep(3);
         request
             .post('http://localhost:1337')
             .send("")
@@ -64,11 +65,79 @@ describe('handleAcknowledge', () => {
             .set('Content-Type', 'application/pdf')
             .set('Access-Control-Allow-Origin', '*')
             .end((err, res) => {
-                expect(res.header['content-type']).to.equal('application/pdf')
-                expect(res.header['access-control-allow-origin']).to.equal('*')
-                done()
+                expect(res.header['content-type']).to.equal('application/pdf');
+                expect(res.header['access-control-allow-origin']).to.equal('*');
+                done();
                 server.kill('SIGTERM')
             })
+
+    }).timeout(7000)
+
+    it('response should have correct headers', done => {
+        var server = process.exec('node ./src/index.js',
+            (error, stdout, stderr) => {
+                // console.log('stdout: ', stdout);
+                // console.log('stderr: ', stderr);
+                // if (error !== null) {
+                //     console.log('exec error: ', error);
+                // }
+            });
+
+        sleep(3);
+        request
+            .post('http://localhost:1337')
+            .send("")
+            .timeout({
+                response: 5000,  // Wait 5 seconds for the server to start sending,
+                deadline: 60000, // but allow 1 minute for the file to finish loading.
+            })
+            .set('Content-Type', 'application/pdf')
+            .set('Access-Control-Allow-Origin', '*')
+            .end((err, res) => {
+                expect(res.header['content-type']).to.equal('application/pdf');
+                expect(res.header['access-control-allow-origin']).to.equal('*');
+                done();
+                server.kill('SIGTERM')
+            })
+
+    }).timeout(7000)
+
+    it('should respond with the correct file', done => {
+        var server = process.exec('node ./src/index.js',
+            (error, stdout, stderr) => {
+                // console.log('stdout: ', stdout);
+                // console.log('stderr: ', stderr);
+                // if (error !== null) {
+                //     console.log('exec error: ', error);
+                // }
+            });
+
+        const filePath = path.join(__dirname, './resource/index.html');
+
+        fs.readFile(filePath, {encoding: 'utf-8'}, (err, data) => {
+            sleep(3);
+            if (err) {
+                console.log('exec error: ', err)
+            }
+
+            request
+                .post('http://localhost:1337')
+                .send(data)
+                .timeout({
+                    response: 8000,  // Wait 5 seconds for the server to start sending,
+                    deadline: 60000, // but allow 1 minute for the file to finish loading.
+                })
+                .set('Content-Type', 'application/pdf')
+                .set('Access-Control-Allow-Origin', '*')
+                .buffer(true)
+                .parse(request.parse.image)
+                .then(res => res.body)
+                .then(res => {
+                    expect(res.toString('utf-8').split('\n')[0]).to.equal('%PDF-1.4');
+                    done();
+                    server.kill('SIGTERM')
+                })
+        })
 
     }).timeout(7000)
 })
